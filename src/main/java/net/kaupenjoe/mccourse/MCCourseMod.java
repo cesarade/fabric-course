d9b42135ccb8838bca513860b68d43590bfda112
César Alcívar <cesar_ade@hotmail.com>
2022-05-05 12:33:11 -0500

    block item

--- src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
+++ src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
@@ -1,6 +1,8 @@
 package net.kaupenjoe.mccourse;
 
 import net.fabricmc.api.ModInitializer;
+import net.kaupenjoe.mccourse.block.ModBlocks;
+import net.kaupenjoe.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,10 +13,7 @@ public class MCCourseMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+		ModBlocks.registerModBlock();
 	}
 }
---
+++ src/main/java/net/kaupenjoe/mccourse/item/ModFoodComponents.java
@@ -0,0 +1,21 @@
+package net.kaupenjoe.mccourse.item;
+
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
+
+public class ModFoodComponents {
+
+//    public static FoodComponent encebollado = new FoodComponent.Builder()
+//            .hunger(2)
+//            .saturationModifier(0.3f)
+//            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 200), 1f)
+//            .build();
+
+    public static FoodComponent ENCEBOLLADO = new FoodComponent.Builder()
+            .hunger(2)
+            .saturationModifier(0.3f).build();
+
+
+}
